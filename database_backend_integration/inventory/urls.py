699fd28eabac5c6f7ae88cf8e9d679bc8851e2b3
Kıvanç Filizci <e239915@metu.edu.tr>
2025-04-05 16:36:40 +0300

    sprint-7 item-1 : backend-inventory-urls completed

--- database_backend_integration/inventory/urls.py
+++ database_backend_integration/inventory/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ReadyViewSet
+from .views import (
+    ReadyViewSet, AtpStockViewSet, IntransitViewSet,
+    ToBeProducedViewSet, SalesViewSet, UsersViewSet
+)
 
 router = DefaultRouter()
-router.register(r'ready', ReadyViewSet)  # Adjust name
+router.register(r'ready', ReadyViewSet)
+router.register(r'atp-stock', AtpStockViewSet)
+router.register(r'intransit', IntransitViewSet)
+router.register(r'to-be-produced', ToBeProducedViewSet)
+router.register(r'sales', SalesViewSet)
+router.register(r'users', UsersViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
