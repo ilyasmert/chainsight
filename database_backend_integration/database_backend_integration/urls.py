834fdc933b41a7f8edaf26602eb09143b40abf36
Kıvanç Filizci <e239915@metu.edu.tr>
2025-04-05 22:13:12 +0300

    sprint-7 item-1 : backend-CRUD-operations completed

--- database_backend_integration/database_backend_integration/urls.py
+++ database_backend_integration/database_backend_integration/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),  # <- this enables the login/logout UI
     path('api/inventory/', include('inventory.urls')),
 ]
