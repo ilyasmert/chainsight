b9bf15a788e895ad21458997983d4675fefdc759
Kıvanç Filizci <e239915@metu.edu.tr>
2025-04-05 16:31:28 +0300

    sprint-7 item-1 : backend-inventory-views completed

--- database_backend_integration/inventory/views.py
+++ database_backend_integration/inventory/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Ready
-from .serializers import ReadySerializer
+from .models import (
+    Ready, AtpStock, Intransit, ToBeProduced, Sales, Users
+)
+from .serializers import (
+    ReadySerializer, AtpStockSerializer, IntransitSerializer,
+    ToBeProducedSerializer, SalesSerializer, UsersSerializer
+)
 
-class ReadyViewSet(viewsets.ModelViewSet):
-    queryset = Ready.objects.all()
-    serializer_class = ReadySerializer
+def create_viewset(model, serializer):
+    class GenericViewSet(viewsets.ModelViewSet):
+        queryset = model.objects.all()
+        serializer_class = serializer
+    return GenericViewSet
 
-# Create your views here.
+ReadyViewSet = create_viewset(Ready, ReadySerializer)
+AtpStockViewSet = create_viewset(AtpStock, AtpStockSerializer)
+IntransitViewSet = create_viewset(Intransit, IntransitSerializer)
+ToBeProducedViewSet = create_viewset(ToBeProduced, ToBeProducedSerializer)
+SalesViewSet = create_viewset(Sales, SalesSerializer)
+UsersViewSet = create_viewset(Users, UsersSerializer)
